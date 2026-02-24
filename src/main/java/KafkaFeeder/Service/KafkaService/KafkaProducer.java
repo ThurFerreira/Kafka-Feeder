1f0dbe248b0a094d96c159ce673c2fb11e434a5b
arthurferreirafdev <arthurfsilva110@gmail.com>
2026-02-24 17:19:31 -0300

    feat: sender algorithm (full broken)

--- src/main/java/KafkaFeeder/Service/KafkaService/KafkaProducer.java
+++ src/main/java/KafkaFeeder/Service/KafkaService/KafkaProducer.java
@@ -1,5 +1,6 @@
 package KafkaFeeder.Service.KafkaService;
 
+import KafkaFeeder.model.Message;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
@@ -11,17 +12,26 @@ import org.springframework.stereotype.Service;
 import static com.fasterxml.jackson.databind.SerializationFeature.WRITE_DATES_AS_TIMESTAMPS;
 
 @Component
-public class KafkaProducer {
+public class KafkaProducer implements Runnable {
 
     private final ObjectMapper objectMapper;
     private final KafkaTemplate<String, String> kafkaTemplate;
+    private Message message;
 
     public KafkaProducer(ObjectMapper objectMapper, KafkaTemplate<String, String> kafkaTemplate) {
         this.objectMapper = objectMapper;
         this.kafkaTemplate = kafkaTemplate;
     }
 
-    public void Send(Object message, String topic, String key) throws JsonProcessingException {
-        kafkaTemplate.send(topic, key, objectMapper.writeValueAsString(message));
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                kafkaTemplate.send(message.topic(), message.key(), objectMapper.writeValueAsString(message));
+            } catch (JsonProcessingException e) {
+                throw new RuntimeException(e);
+            }
+
+        }
     }
 }
--- src/main/java/KafkaFeeder/controller/MessageController.java
+++ src/main/java/KafkaFeeder/controller/MessageController.java
@@ -1,14 +1,15 @@
 package KafkaFeeder.controller;
 
 import KafkaFeeder.Service.KafkaService.KafkaProducer;
-import KafkaFeeder.model.ProduceMessageDto;
+import KafkaFeeder.model.Headers;
+import KafkaFeeder.model.Message;
+import KafkaFeeder.model.StopConditions;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
@@ -18,8 +19,9 @@ public class MessageController {
     @Autowired
     private KafkaProducer producer;
 
-    @PostMapping
-    public ResponseEntity<Object> ProduceMessage(@RequestParam ProduceMessageDto produceMessageDto) {
-        producer
+    @PostMapping("")
+    public ResponseEntity<Object> ProduceMessage(@RequestParam @NotNull Message message) {
+
+        return new ResponseEntity<>(body, HttpStatus.OK);
     }
 }
